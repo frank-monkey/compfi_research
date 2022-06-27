d2bb8ac2c9ef0e0d0baf835b15207888b7e608ac
Frank Sacco <fsacco@andrew.cmu.edu>
2022-06-27 12:44:08 -0400

    added option pricing methods

--- general_modelling.py
+++ general_modelling.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 np.set_printoptions(suppress=True)
 
-def binomial_pricing(S_0, n, u, d):
+#modelling methods
+def binomial_model(S_0, n, u, d):
     prices = np.zeros((n+1, n+1))
     prices[0][0] = S_0
     for j in range(1,n+1):
@@ -13,7 +14,7 @@ def binomial_pricing(S_0, n, u, d):
     print(prices)
     return prices
 
-def bachelier_pricing(S_0, n, mu, beta): 
+def bachelier_model(S_0, n, mu, beta): 
     prices = np.zeros((n+1, n+1))
     prices[0][0] = S_0
     for j in range(1,n+1):
@@ -23,7 +24,7 @@ def bachelier_pricing(S_0, n, mu, beta):
     print(prices)
     return prices
 
-def arithmetic_bachelier_pricing(S_0, n, alpha, beta): 
+def arithmetic_bachelier_model(S_0, n, alpha, beta): 
     prices = np.zeros((n+1, n+1))
     prices[0][0] = S_0
     for j in range(1,n+1):
@@ -33,4 +34,21 @@ def arithmetic_bachelier_pricing(S_0, n, alpha, beta):
     print(prices)
     return prices
 
+#option pricing methods
+def call(S, K):
+    return max(0, S[0] - K)
 
+def put(S, K):
+    return max(0, K - S[0])
+
+def chooser(S, K):
+    return max(S[0] - K, K - S[0])
+
+def squared_price(S, K):
+    return S[0]**2
+
+def summation_root(S, K):
+    return np.sum(S)**(1/len(S))
+
+def product_root(S, K):
+    return np.prod(S)**(1/len(S))
