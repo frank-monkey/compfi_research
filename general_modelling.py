79bfa2ad9cf93fcc09aa5248113ed363a66f8ea5
Frank Sacco <fsacco@andrew.cmu.edu>
2022-06-29 13:18:16 -0400

    made path_dependent probabilities non-constant

--- general_modelling.py
+++ general_modelling.py
@@ -52,6 +52,15 @@ def summation_root(S, K):
 def product_root(S, K):
     return np.prod(S)**(1/len(S))
 
+def asian_call(S, K):
+    return call(np.sum(S)/len(S),K)
+
+def asian_put(S, K):
+    return put(np.sum(S)/len(S),K)
+
+def asian_chooser(S, K):
+    return chooser(np.sum(S)/len(S),K)
+
 def path_independent_pricing(prices, option_pricing, K, p, n, r):
     q=1-p
     op = lambda z : option_pricing(z, K)
@@ -65,17 +74,24 @@ def path_independent_pricing(prices, option_pricing, K, p, n, r):
     print(V)
     print(V[0][0])
 
-def path_dependent_pricing(prices, option_pricing, K, p, n):
-    q=1-p
+def path_dependent_pricing(prices, option_pricing, K, n, r):
+    probabilities = np.zeros((n, n))
+    for j in range(0,n):
+        for i in range(0, j+1):
+            probabilities[i][j] =  ((1+r)*prices[i][j]-prices[i][j+1])/(prices[i+1][j+1]-prices[i][j+1])
+    print(probabilities)
+
     sum = 0
     op = lambda z : option_pricing(z, K)
-    combinations = [list('{:03b}'.format(x)) for x in range(2**n)] #generates a list of 0s and 1s to represent paths possible
-
+    combinations = [list(('{:0' + str(n) + 'b}').format(x)) for x in range(2**n)] #generates a list of 0s and 1s to represent paths possible
+    #print(combinations)
     for comb in combinations:
         probability = 1
         ret_prices = [] #holds pathing of prices to put into pricing method
         row, col = 0, 0
         for i in comb:
+            p = probabilities[row][col]
+            q = p-1
             if i=='0':
                 probability*=p
                 row+=1
@@ -85,9 +101,12 @@ def path_dependent_pricing(prices, option_pricing, K, p, n):
                 probability*=q
                 col+=1
                 ret_prices.append(prices[row][col])
+        #print(ret_prices)
         sum+=probability*op(ret_prices)
-    print(sum)
+        print(ret_prices)
+    print(((1/(r+1))**n*sum))
 
-path_dependent_pricing(bachelier_model(10, 5, 0.3, 0.2), product_root, -1, 0.5, 5) 
+path_dependent_pricing(bachelier_model(10, 5, 1, 3), product_root, -1, 5, 0.03) 
 #K doesnt matter so I made it -1
+#path_dependent_pricing(binomial_model(8, 10, 2, 0.5), asian_call, 8, 10, 0.25) 
 #path_dependent_pricing(arithmetic_bachelier_model(10, 5, 0.3, 0.2), product_root, -1, 0.5, 5) 
