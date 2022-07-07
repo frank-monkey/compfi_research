12de1d6fe0422fe31f40578d9b01f4676e139ae4
Frank Sacco <fsacco@andrew.cmu.edu>
2022-07-07 12:55:06 -0400

    chooser options

--- general_modelling.py
+++ general_modelling.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pyparsing import alphanums
 import pricing_methods
 np.set_printoptions(suppress=True, precision=3)
 
@@ -33,9 +34,9 @@ def arithmetic_bachelier_model(S_0, n, alpha, beta):
     print(prices)
     return prices
 
-def path_independent_pricing(prices, option_pricing, K, p, n, r):
+def path_independent_pricing(prices, option_pricing, K, p, n, r, t=0):
     q=1-p
-    op = lambda z : option_pricing(z, K)
+    op = lambda z : option_pricing(z, K, t)
     V = np.zeros((n+1, n+1))
     for j in range(0, n):
         V[j][n] = op(prices[j][n])
@@ -46,7 +47,7 @@ def path_independent_pricing(prices, option_pricing, K, p, n, r):
     print(V)
     print(V[0][0])
 
-def path_dependent_pricing(prices, option_pricing, K, n, r):
+def path_dependent_pricing(prices, option_pricing, K, n, r, t=0):
     probabilities = np.zeros((n, n)) #chance of going up each seperate move
     for j in range(0,n):
         for i in range(0, j+1):
@@ -54,7 +55,7 @@ def path_dependent_pricing(prices, option_pricing, K, n, r):
     print(probabilities)
 
     sum = 0
-    op = lambda z : option_pricing(z, K)
+    op = lambda z : option_pricing(z, K, t)
     combinations = [list(('{:0' + str(n) + 'b}').format(x)) for x in range(2**n)] #generates a list of 0s and 1s to represent paths possible
     #print(combinations)
     for comb in combinations:
@@ -74,9 +75,12 @@ def path_dependent_pricing(prices, option_pricing, K, n, r):
                 col+=1
                 ret_prices.append(prices[row][col])
         sum += probability * op(ret_prices)
+    print("interest-free price = " + str(sum))
     return (((1/(r+1))**n * sum))
 
-print(path_dependent_pricing(binomial_model(10, 2, 2, 0.5), pricing_methods.geometric_asian_call, 10, 2, 0.25) )
+#print(path_dependent_pricing(binomial_model(10, 2, 2, 0.5), pricing_methods.geometric_asian_call, 10, 2, 0.25) )
+print(path_dependent_pricing(binomial_model(10, 2, 2, 0.5), pricing_methods.arithmetic_asian_call, 10, 2, 0.25, 1) )
+print(path_dependent_pricing(bachelier_model(10, 10, 1, 2), pricing_methods.arithmetic_asian_call, 10, 10, 0.25, 1) )
 
 #path_dependent_pricing(bachelier_model(10, 10, 1, 2), product_root, -1, 10, 0.03) 
 #path_dependent_pricing(binomial_model(8, 10, 2, 0.5), asian_call, 8, 10, 0.25) 
--- pricing_methods.py
+++ pricing_methods.py
@@ -9,31 +9,37 @@ def put(S_0, K):
 def chooser(S_0, K):
     return max(S_0 - K, K - S_0)
 
-def squared_price(S, K):
-    return S**2
+def squared_price(S_0, K):
+    return S_0**2
 
 #path dependent options
-def summation_root(S, K):
+def summation_root(S, K, t):
     return max(0,np.sum(S)**(1/len(S)))
 
-def product_root(S, K):
+def product_root(S, K, t):
     return max(0,np.prod(S)**(1/len(S)))
 
-def arithmetic_asian_call(S, K):
+def arithmetic_asian_call(S, K, t):
+    S=S[t:]
     return call(np.sum(S)/len(S),K)
 
-def arithmetic_asian_put(S, K):
+def arithmetic_asian_put(S, K, t):
+    S=S[t:]
     return put(np.sum(S)/len(S),K)
 
-def arithmetic_asian_chooser(S, K):
+def arithmetic_asian_chooser(S, K, t):
+    S=S[t:]
     return chooser(np.sum(S)/len(S),K)
 
-def geometric_asian_call(S, K):
-    #print(np.prod(S)**(1/len(S)))
+def geometric_asian_call(S, K, t):
+    S=S[t:]
     return call(np.prod(S)**(1/len(S)),K)
 
-def geometric_asian_put(S, K):
+def geometric_asian_put(S, K, t):
+    S=S[t:]
     return put(np.prod(S)**(1/len(S)),K)
 
-def geometric_asian_chooser(S, K):
+def geometric_asian_chooser(S, K, t):
+    S=S[t:]
     return chooser(np.prod(S)**(1/len(S)),K)
+    
