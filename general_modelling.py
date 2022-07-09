a0349f9fe1eca33205ef47cd022cf169b3780180
Frank Sacco <fsacco@andrew.cmu.edu>
2022-07-09 13:31:02 -0400

    primitive fix for path dependent pricing

--- general_modelling.py
+++ general_modelling.py
@@ -1,86 +1,81 @@
 import numpy as np
-from pyparsing import alphanums
 import pricing_methods
 np.set_printoptions(suppress=True, precision=3)
 
 #modelling methods
 def binomial_model(S_0, n, u, d):
-    prices = np.zeros((n+1, n+1))
+    prices = np.zeros((2**n, n+1))
     prices[0][0] = S_0
-    for j in range(1,n+1):
-        prices[j][j]=u*prices[j-1][j-1]
-        for i in range(0, j):
-            prices[i][j] = d*prices[i][j-1]
+    for j in range(0, n):
+        for i in range(0, 2**j):
+            prices[2*i+1][j+1] = u*prices[i][j] #up case
+            prices[2*i][j+1] = d*prices[i][j] #down case
     print(prices)
     return prices
 
 def bachelier_model(S_0, n, mu, beta): 
-    prices = np.zeros((n+1, n+1))
+    prices = np.zeros((2**n, n+1))
     prices[0][0] = S_0
-    for j in range(1,n+1):
-        prices[j][j]=mu*(1+prices[j-1][j-1])+beta
-        for i in range(0, j):
-            prices[i][j] = mu*(1+prices[i][j-1])-beta
+    for j in range(0, n):
+        for i in range(0, 2**j):
+            prices[2*i+1][j+1] = mu*prices[i][j]+beta #up case
+            prices[2*i][j+1] = mu*prices[i][j]-beta #down case
     print(prices)
     return prices
 
 def arithmetic_bachelier_model(S_0, n, alpha, beta): 
-    prices = np.zeros((n+1, n+1))
+    prices = np.zeros((2**n, n+1))
     prices[0][0] = S_0
-    for j in range(1,n+1):
-        prices[j][j]=prices[j-1][j-1]+alpha+beta
-        for i in range(0, j):
-            prices[i][j] = prices[i][j-1]+alpha-beta
+    for j in range(0, n):
+        for i in range(0, 2**j):
+            prices[2*i+1][j+1] = prices[i][j]+alpha+beta #up case
+            prices[2*i][j+1] = prices[i][j]+alpha-beta #down case
     print(prices)
     return prices
 
-def path_independent_pricing(prices, option_pricing, K, p, n, r, t=0):
-    q=1-p
-    op = lambda z : option_pricing(z, K, t)
-    V = np.zeros((n+1, n+1))
-    for j in range(0, n):
-        V[j][n] = op(prices[j][n])
-
-    for j in range(n-1, -1, -1):
-        for i in range(n-1, j-1, -1):
-            V[j][i] = (1/(r+1))*(p*V[j][i+1]+q*V[j+1][i+1])
-    print(V)
-    print(V[0][0])
-
 def path_dependent_pricing(prices, option_pricing, K, n, r, t=0):
-    probabilities = np.zeros((n, n)) #chance of going up each seperate move
+    probabilities = np.zeros((2**(n-1), n)) #p_hat of each move
     for j in range(0,n):
-        for i in range(0, j+1):
-            probabilities[i][j] =  ((1+r)*prices[i][j]-prices[i][j+1])/(prices[i+1][j+1]-prices[i][j+1])
+        for i in range(0, 2**j):
+            S_0 = prices[i][j]
+            up = prices[2*i+1][j+1]
+            down = prices[2*i][j+1]
+            probabilities[i][j] =  ((1+r)*S_0-down)/(up-down)
     print(probabilities)
 
     sum = 0
     op = lambda z : option_pricing(z, K, t)
     combinations = [list(('{:0' + str(n) + 'b}').format(x)) for x in range(2**n)] #generates a list of 0s and 1s to represent paths possible
     #print(combinations)
-    for comb in combinations:
+    for i in range(2**n):
+        comb = ('{:0' + str(n) + 'b}').format(i) #outputs combination of '1's and '0's
         probability = 1
         ret_prices = [] #holds pathing of prices to put into pricing method
         row, col = 0, 0
         for i in comb:
             p = probabilities[row][col]
-            q = 1-p
-            if i=='0':
+            q = 1 - p
+            if i=='0': #up
                 probability*=p
-                row+=1
+                row=2*row+1
                 col+=1
                 ret_prices.append(prices[row][col])
-            else:
+            else: #down
                 probability*=q
+                row*=2
                 col+=1
                 ret_prices.append(prices[row][col])
         sum += probability * op(ret_prices)
     print("interest-free price = " + str(sum))
     return (((1/(r+1))**n * sum))
 
-#print(path_dependent_pricing(binomial_model(10, 2, 2, 0.5), pricing_methods.geometric_asian_call, 10, 2, 0.25) )
-print(path_dependent_pricing(binomial_model(10, 2, 2, 0.5), pricing_methods.arithmetic_asian_call, 10, 2, 0.25, 1) )
-print(path_dependent_pricing(bachelier_model(10, 10, 1, 2), pricing_methods.arithmetic_asian_call, 10, 10, 0.25, 1) )
+#binomial_model(10,4,2,0.5)
+#bachelier_model(10,4,2,0.5)
+#arithmetic_bachelier_model(10,4,2,0.5)
+
+print(path_dependent_pricing(binomial_model(10, 5, 2, 0.5), pricing_methods.geometric_asian_call, 5, 5, 0.25) )
+#print(path_dependent_pricing(binomial_model(10, 2, 2, 0.5), pricing_methods.arithmetic_asian_call, 10, 2, 0.25, 1) )
+#print(path_dependent_pricing(bachelier_model(8, 5, 0.2, 0.5), pricing_methods.arithmetic_asian_call, 5, 5, 0.25, 1) )
 
 #path_dependent_pricing(bachelier_model(10, 10, 1, 2), product_root, -1, 10, 0.03) 
 #path_dependent_pricing(binomial_model(8, 10, 2, 0.5), asian_call, 8, 10, 0.25) 
